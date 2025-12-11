a240ec7f20e2a37767d1364f6292ced7c2b1d7f7
Ragav Srinivasan <56784771+Rageon787@users.noreply.github.com>
2025-12-11 22:44:36 +1100

    ``run_file()`` file existence check and read contents from file.

--- main.cpp
+++ main.cpp
@@ -1,35 +1,64 @@
-#include <iostream>
-using namespace std;
+#include<iostream>
+#include<filesystem>
+#include<fstream>
+#include<vector>
+#include<string>
+namespace fs = std::filesystem;
+
 
 class Lox {
 
+private:
 public:
+    static std::vector<std::string> source; // store source code as a vector of strings
 
-    static void run_file() {
-        cout <<  "Interpreter will be run on a file\n";
+    static void run_file(const fs::path& given_path) {
+        std::cout << given_path << std::endl;
+        // File existence check
+        if (fs::exists(given_path)) {
+            // file exists: read the source from the file and proceed to scanning
+            std::cout << "file exists" << std::endl;
+            std::ifstream file_reader(given_path);
+
+            // read the source code from the file
+            std::string line;
+            while (getline(file_reader, line)) {
+                source.push_back(line);
+            }
+
+
+        } else {
+            std::cout << "file doesn't exist" << std::endl;
+            // File doesn't exist: throw an error to the user
+        }
     }
 
     static void run_prompt() {
         while (true) {
-            cout << "> ";
-            string prompt;
-            getline(cin, prompt);
-            cout << prompt << endl;
+            std::cout << "> ";
+            std::string prompt;
+            getline(std::cin, prompt);
+            std::cout << prompt << std::endl;
+            // TODO: need to scan this prompt and generate a series of tokens
         }
-
     }
+
+
 };
+std::vector<std::string> Lox::source;
 
 int main(int argc, char* argv[]) {
 
-    Lox lox;
+    Lox lox = Lox();
 
-    if (argc == 1) {
-        // no arguments are present: REPL Mode
+    argc--; // decrement the program argument
+
+    if (argc == 0) {
+        // no arguments are present: Prompt Mode
         lox.run_prompt();
-    } else if (argc == 2) {
-        // If there is only one argument: run file
-        lox.run_file();
+    } else if (argc == 1) {
+        // If there is only one argument: File mode
+        lox.run_file(argv[1]);
     } else {
         // if there is more than one arg: Exit
     }
--- notes.md
+++ notes.md
@@ -1,25 +1,24 @@
+# Scanning 
+
+Our first 
+## Tokenization of source code 
+
+Consider this piece of lox code: 
+```
+fun print_hello(var x) {
+  for (var i = 0; i < 5; i++) {
+    print "hello world";
+  }
+  return;
+}
+```
 
 
-- To study Interpreter concepts 
-- To understand java code from the crafting interpreters book 
-- translate them to C++ code
 
 
-# Chapter 4: Scanning 
 
-### 4.1: Interpreter Framework
 
-**Code Snippet 1** 
-- Look at the number of arguments 
-- if the number of arguments is zero  
-  - Run in REPL mode 
-  - call ``run_prompt()``
-- if the number of arguments is one 
-  - Possible lox file
-  - But still need to check if it has the correct extension
-  - but if it is: ``run_file()``
-- More than two arguments 
-  - We exit the prograjm
+
 
 
 
